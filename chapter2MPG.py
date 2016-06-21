ae8ab525ecc6751a325c2c6790e9539917b60bf8
Jeff Masterson <j.mastersonjr@gmail.com>
2016-06-20 23:18:15 -0400

    Add files via upload
    The task for this python project was "A car’s miles-per-gallon (MPG) can be calculated with the following formula:
    
    
    MPG = Miles driven / Gallons of gas used
    
    
    Write a program that asks the user for the number of miles driven and the gallons of gas 
    used. It should calculate the car’s miles-per-gallon and display the result."

---
+++ chapter2MPG.py
@@ -0,0 +1,14 @@
+#Jeff Masterson
+#Chapter 2 #2
+
+#Get the miles driven.
+miles_driven = float(input('Enter the Miles Driven: '))
+
+#Get the gallons of gas used.
+gallons = float(input('Enter the ammount of gallons used: '))
+
+#calculate the profit as 23 percent of total sales.
+mpg = miles_driven /gallons
+
+# Display the profit.
+print('The MPG is', format(mpg,))
